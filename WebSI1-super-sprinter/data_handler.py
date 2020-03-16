26ded77cf137166e2d75cdfd5b020df943aac6b4
allused <divixed12@gmail.com>
2020-03-16 12:51:08 +0100

    all

---
+++ WebSI1-super-sprinter/data_handler.py
@@ -0,0 +1,67 @@
+import csv
+import os
+import random
+import string
+
+DATA_FILE_PATH = os.getenv('DATA_FILE_PATH') if 'DATA_FILE_PATH' in os.environ else 'data.csv'
+DATA_HEADER = ['id', 'title', 'user_story', 'acceptance_criteria', 'business_value', 'estimation', 'status']
+STATUSES = ['planning', 'todo', 'in progress', 'review', 'done']
+
+
+def read_table_from_file(file_name, separator=';'):
+    """Read CSV file into a data table.
+
+    Args:
+        file_name: The name of the CSV data file.
+        separator: The CSV separator character
+
+    Returns:
+        The data parsed into a list of lists.
+    """
+    try:
+        with open(file_name, "r") as file:
+            lines = file.readlines()
+        return [element.replace("\n", "").split(separator) for element in lines]
+    except IOError:
+        return []
+
+def write_table_to_file(file_name, table, separator=';'):
+    """Write tabular data into a CSV file.
+
+    Args:
+        file_name: The name of the file to write to.
+        table: list of lists containing tabular data.
+        separator: The CSV separator character
+    """
+    with open(file_name, "w") as file:
+        for record in table:
+            row = separator.join(record)
+            file.write(row + "\n")
+
+def make_dict(data, headers):
+    return [{headers[i]: stat for i, stat in enumerate(line)} for line in data]
+
+def make_id(dicts):
+    id = []
+    id.append(random.choice(string.ascii_letters))
+    id.append(random.choice(string.digits))
+    for dict in dicts:
+        if dict['id'] != id:
+            return "".join(id)
+
+def get_all_user_story():
+    return []
+
+def make_list(data):
+    save_list = []
+    for dict in data:
+        current_list = []
+        current_list.extend(dict.values())
+        save_list.append(current_list)
+    return save_list
+
+
+
+data_list = read_table_from_file(DATA_FILE_PATH,';')
+data_dict = make_dict(data_list, DATA_HEADER)
+print(make_list(data_dict))
---
+++ WebSI1-super-sprinter/server.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+import data_handler
+
+app = Flask(__name__)
+data = data_handler.data_dict
+headers = data_handler.DATA_HEADER
+
+
+@app.route('/')
+def main_page():
+    data = data_handler.data_dict
+
+
+
+    return render_template('mainpage.html', data=data, headers=headers)
+
+
+
+@app.route('/story', methods=['GET','POST'])
+def route_story():
+    user_stories = data_handler.get_all_user_story()
+    actual_story = {}
+
+    if request.method == 'POST':
+        actual_story['id'] = data_handler.make_id(data)
+
+        for i in range(1,6):
+            actual_story[headers[i]] = request.form[headers[i]]
+        actual_story['status'] = data_handler.STATUSES[0]
+
+        data.append(actual_story)
+
+        tosave = data_handler.make_list(data)
+        data_handler.write_table_to_file(data_handler.DATA_FILE_PATH,tosave)
+        actual_story = {}
+        return redirect('/')
+
+
+    return render_template('story.html', user_stories=user_stories)
+
+@app.route('/story/<id>')
+def story_id(id):
+    pass
+
+if __name__ == '__main__':
+    data
+    app.run(
+        host='0.0.0.0',
+        port=8000,
+        debug=True,
+    )
